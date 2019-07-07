82c2976c2b2ff37c1b070df874eea49412224b0f
developersloop <loopinfinite.all@gmail.com>
2019-07-07 19:01:45 -0300

    .

---
+++ src/components/Table/Table.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <table class="table" id="users">
+             <slot name="thead"  class="thead-dark text-center"></slot>
+             <slot name="tbody"></slot>
+        </table>
+    </div>
+</template>
+
+<script>
+     export default {
+         data(){
+             return {
+
+             }
+         }
+     }
+</script>
